2f1e15013d410bdb2602a43b93c759d334b80388
Ak2hay-1 <patilakshayrkd@gmail.com>
2026-07-12 11:07:30 +0530

    feat(core): add application constants

---
+++ backend/app/core/constants.py
@@ -0,0 +1,49 @@
+"""
+Application constants.
+"""
+
+from __future__ import annotations
+
+APP_AUTHOR = "Akshay Patil"
+
+DEFAULT_SYMBOL = "XAUUSD"
+
+SUPPORTED_SYMBOLS = [
+    "XAUUSD",
+    "EURUSD",
+    "GBPUSD",
+    "USDJPY",
+    "BTCUSD",
+    "ETHUSD",
+]
+
+SUPPORTED_TIMEFRAMES = [
+    "M1",
+    "M5",
+    "M15",
+    "M30",
+    "H1",
+    "H4",
+    "D1",
+]
+
+DEFAULT_TIMEFRAME = "M5"
+
+DEFAULT_CANDLE_LIMIT = 500
+
+MAX_CANDLE_LIMIT = 5000
+
+DEFAULT_LOT_SIZE = 0.01
+
+MAX_RISK_PERCENT = 2.0
+
+API_VERSION = "v1"
+
+LOG_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+LOG_FORMAT = (
+    "%(asctime)s | "
+    "%(levelname)-8s | "
+    "%(name)s | "
+    "%(message)s"
+)
